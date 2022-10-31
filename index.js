99f14e7ca939233cc8a4511d3338cf9a98b19724
fabio <fabiomirza@outlook.com>
2022-10-31 21:00:59 +0100

    changes

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ const app = express();
 app.listen(process.env.PORT || 5000, () => console.log(`Listening on 5000`));
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  // res.send("hello world");
+  console.log(req.query.campaignId);
+  console.log(req.query.workerId);
+  res.send(
+    `campaignID is ${req.query.campaignId} and workerID id ${req.query.workerId}`
+  );
 });

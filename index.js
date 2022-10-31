8b0f84b22bb76229cfbe98526529ad179eebb984
fabio <fabiomirza@outlook.com>
2022-10-31 20:51:57 +0100

    again

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 import express from "express";
 const app = express();
+
+app.listen(process.env.PORT || 5000, () => console.log(`Listening on 5000`));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});

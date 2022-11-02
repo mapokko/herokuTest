9923f348152fc7c338d260d2d948c5a67ca1d7f5
fabio <fabiomirza@outlook.com>
2022-11-02 14:06:30 +0100

    .

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+import axios from "axios";
 import express from "express";
 const app = express();
+
+const apiKey =
+  "dbe39902202c25ac81556135f5fc2d610c3522bc6ce5fc83f0ddc24ce2fe4d0a";
 
 app.listen(process.env.PORT || 5000, () => console.log(`Listening on 5000`));
 
@@ -15,6 +19,37 @@ app.get("/", (req, res) => {
   );
 });
 
-app.get("/external", (req, res) => {
-  res.send(`slot id is ${req.query.slotId}`);
+app.get("/external", async (req, res) => {
+  res.send(
+    `slot id is ${req.query.slotId} <a href="/getPayed?slotId=${req.query.slotId}" >get payed</a>`
+  );
+});
+
+app.get("/getPayed", async (req, res1) => {
+  const config = {
+    headers: {
+      MicroworkersApiKey: apiKey,
+      "Content-Type": "application/json",
+    },
+  };
+
+  await axios
+    .put(
+      `https://ttv-sandbox.microworkers.com/api/v2/slots/${req.query.slotId}/submitProof`,
+      {
+        answers: [
+          {
+            key: "keyName",
+            value: "valueName",
+          },
+        ],
+      },
+      config
+    )
+    .then((res) => {
+      res1.send(`you got payed at ${req.query.slotId}`);
+    })
+    .catch((err) => {
+      res1.send(`you DID NOT get payed at ${req.query.slotId}`);
+    });
 });
